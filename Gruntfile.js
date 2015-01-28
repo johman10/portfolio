59b0b0f46462fc7ca4ea5392994f1886880fd6dd
Johman10 <johveck@gmail.com>
2015-01-28 17:27:18 +0100

    Add modernizr + Grunt improvements + style improvements

--- Gruntfile.js
+++ Gruntfile.js
@@ -2,22 +2,51 @@ module.exports = function(grunt) {
   require('load-grunt-tasks')(grunt);
 
   grunt.loadNpmTasks('grunt-contrib-compass');
+  grunt.loadNpmTasks('grunt-autoprefixer');
+  grunt.loadNpmTasks('grunt-modernizr');
+  grunt.loadNpmTasks('grunt-contrib-watch');
 
   grunt.initConfig({
     compass: {
       dist: {
         options: {
           sassDir: 'style/scss',
+          specify: 'style/scss/main.scss',
           cssDir: 'style/css',
           imagesDir: 'img',
           javascriptsDir: 'js',
           fontsDir: 'style/fonts',
-          outputStyle: 'compressed',
-          watch: true
+          outputStyle: 'compressed'
         }
       }
-    }
+    },
+    autoprefixer: {
+      options: {
+        browsers: ['last 2 versions', 'ie 8', 'ie 9']
+      },
+      dist: {
+        src: 'style/css/main.css',
+        dest: 'style/css/main-prefixed.css'
+      }
+    },
+    modernizr: {
+      dist: {
+        devFile: 'js/modules/modernizr.js',
+        outputFile: 'js/modules/build-modernizr.js',
+        parseFiles: true,
+      }
+    },
+    watch: {
+      scripts: {
+        files: ['**/*.js'],
+        tasks: ['modernizr'],
+      },
+      css: {
+        files: ['**/*.scss'],
+        tasks: ['compass', 'autoprefixer', 'modernizr'],
+      }
+    },
   });
 
-  grunt.registerTask('default', 'compass');
+  // grunt.registerTask('default', ['compass', 'autoprefixer', 'modernizr']);
 };
