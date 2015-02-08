bcb4d781dcc59d6ed0c46dfc3276946dee167741
Johman10 <johveck@gmail.com>
2015-02-08 14:21:40 +0100

    Work animation fallback

--- js/script.js
+++ js/script.js
@@ -1,5 +1,9 @@
 $(document).ready(function() {
   if (!Modernizr.cssanimations) {
+    $('.work_content_container').css({
+      bottom: -400,
+      display: 'block'
+    });
     $( ".home_header" ).delay( 1500 ).animate({
       left: '50%',
       top: '20%'
@@ -8,5 +12,10 @@ $(document).ready(function() {
     $('.home_paragraph').delay(2500).animate({
       opacity: 1
     }, 500);
+
+    $('.work_content_container').delay(2500).animate({
+      bottom: -350,
+      opacity: 1
+    }, 1000);
   }
 });
